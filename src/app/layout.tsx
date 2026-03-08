28f488c0c91d604099dc0f8384236f2e45f8fcd3
Ali <johnjony267@gmail.com>
2026-03-08 17:11:14 +0500

    added monetag script and updated sw.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://quge5.com/88/tag.min.js" data-zone="217601" async data-cfasync="false"></script>
+      </head>
       <body className="bg-background text-foreground antialiased overflow-x-hidden">
         <Navbar />
         <main className="min-h-screen">
